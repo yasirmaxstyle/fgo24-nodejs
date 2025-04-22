ca38b35c92f653fcbae7f1080927e48da9fbf74a
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-22 14:52:15 +0700

    rename file to minitask1.js

---
+++ minitask1.js
@@ -0,0 +1,23 @@
+const { error } = require('console')
+const fs = require('fs')
+const path = require('path')
+
+const listMusic = [
+    'Avenged Sevenfold - Fiction',
+    'Avenged Sevenfold - Save Me',
+    'Avenged Sevenfold - Exist',
+    'Linkin Park - Don\'t Stay',
+    'Linkin Park - Papercut',
+    'Linkin Park - Faint',
+    'Bring Me The Horizon - Sleepwalking',
+    'Bring Me The Horizon - Ludens',
+    'DJ Okawari - Flower Dance',
+    'Kikuo - Dwelling'
+]
+
+listMusic.forEach(music => {
+    fs.writeFile(`${path.join('music/')}${music}.mp3`, "", (err) => {
+        if(!err) console.log('berhasil')
+        else console.log(err)
+    })
+})
